508fe38be54ed3a2f90b866b5566d5995711a772
joshi_lokesh <lokeshjoshijo@gmail.com>
2023-12-06 20:09:41 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div",{id :"grandparent"},React.createElement("div",{id :"parent"},
+
+[React.createElement("div",{id :"child"},[React.createElement("h1",{},"I am H1"),React.createElement("h2",{},"I am H2")]),
+
+React.createElement("div",{id :"child"},[React.createElement("h1",{},"I am H1"),React.createElement("h2",{},"I am H2")])]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(heading); //object
+
+root.render(heading); //render converts object to actual h1 tag that browser understands
+
