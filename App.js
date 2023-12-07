4b26cb5d1af26feeaafaff575ba163d293273f30
joshi_lokesh <lokeshjoshijo@gmail.com>
2023-12-07 16:04:41 +0530

    Second commit

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("div",{id :"grandparent"},React.createElement("div",{id :"parent"},
 
-[React.createElement("div",{id :"child"},[React.createElement("h1",{},"I am H1"),React.createElement("h2",{},"I am H2")]),
+[React.createElement("div",{id :"child"},[React.createElement("h1",{},"I am H23"),React.createElement("h2",{},"I am H2")]),
 
 React.createElement("div",{id :"child"},[React.createElement("h1",{},"I am H1"),React.createElement("h2",{},"I am H2")])]));
 
